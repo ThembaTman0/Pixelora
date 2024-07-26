213b24281025183df1c82ae178d0f33403516a57
ThembaTman0 <thembatman0@gmail.com>
2024-07-26 19:37:28 +0200

    v-0.0.0

---
+++ src/test/java/org/pixelvault/PixelVault/PixelVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pixelvault.PixelVault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PixelVaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
