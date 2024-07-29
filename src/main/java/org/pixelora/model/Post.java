e840a4cbd88a7a499373f8add72c9b9e45395548
ThembaTman0 <thembatman0@gmail.com>
2024-07-29 20:44:06 +0200

    v-0.0.8 H2 DB access

---
+++ src/main/java/org/pixelora/model/Post.java
@@ -0,0 +1,30 @@
+package org.pixelora.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Post {
+
+    @Id
+    @GeneratedValue
+    private  Long id;
+
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String body;
+
+    private LocalDateTime createdAt;
+
+
+}
