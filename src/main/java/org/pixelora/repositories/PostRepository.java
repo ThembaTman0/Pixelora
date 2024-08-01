40bfc9fd2084a47d77f43ddc00596416fc3182d3
ThembaTman0 <thembatman0@gmail.com>
2024-08-01 17:31:35 +0200

    v-0.0.9

---
+++ src/main/java/org/pixelora/repositories/PostRepository.java
@@ -0,0 +1,10 @@
+package org.pixelora.repositories;
+
+import org.pixelora.model.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+
+}
