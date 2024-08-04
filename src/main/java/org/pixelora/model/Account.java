efdb77b34729e91e686f89c189a2be01b7a8b12a
ThembaTman0 <thembatman0@gmail.com>
2024-08-04 20:36:15 +0200

    v-0.1.2

---
+++ src/main/java/org/pixelora/model/Account.java
@@ -0,0 +1,27 @@
+package org.pixelora.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+
+    private  Long id;
+
+    private String email;
+
+    private String password;
+
+    private String firstname;
+}
--- src/main/java/org/pixelora/model/Post.java
+++ src/main/java/org/pixelora/model/Post.java
@@ -1,9 +1,6 @@
 package org.pixelora.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -16,7 +13,7 @@ import java.time.LocalDateTime;
 public class Post {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private  Long id;
 
     private String title;
