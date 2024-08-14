b62da7c23374684645e3ce3fb89740182b24ff32
ThembaTman0 <thembatman0@gmail.com>
2024-08-14 19:29:32 +0200

    v-0.1.7 Working Register Form

--- src/main/java/org/pixelora/Controller/AccountController.java
+++ src/main/java/org/pixelora/Controller/AccountController.java
@@ -1,18 +1,33 @@
 package org.pixelora.Controller;
 
 import org.pixelora.model.Account;
-import org.springframework.ui.Model;
+import org.pixelora.services.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
 
 @Controller
 public class AccountController {
 
+    @Autowired
+    private AccountService accountService;
+
     @GetMapping("/register")
     public String register(Model model){
-
-        Account account  = new Account();
-        model.addAttribute("account",account);
+        Account account = new Account();
+        model.addAttribute("account", account);
         return "register";
     }
+
+    @PostMapping("/register")
+    public String register_user(@ModelAttribute Account account){
+        accountService.save(account);
+        return "redirect:/";
+    }
+
+
 }
