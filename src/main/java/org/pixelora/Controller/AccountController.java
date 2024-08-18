ab046976c56138bd7c8123b732ae3ab288a068ff
ThembaTman0 <thembatman0@gmail.com>
2024-08-18 19:57:16 +0200

    v-0.1.9

--- src/main/java/org/pixelora/Controller/AccountController.java
+++ src/main/java/org/pixelora/Controller/AccountController.java
@@ -14,25 +14,24 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class AccountController {
 
     @Autowired
-    private AccountService accountService;
+    private AccountService accountService;  // Injects an instance of AccountService, which handles business logic for accounts.
 
     @GetMapping("/register")
     public String register(Model model){
-        Account account = new Account();
-        model.addAttribute("account", account);
-        return "register";
+        Account account = new Account();  // Creates a new instance of the Account object.
+        model.addAttribute("account", account);  // Adds the account object to the model to be used in the registration form.
+        return "register";  // Returns the name of the view (register.jsp or register.html) where the form will be displayed.
     }
-    //When registering post the data into DB
+
+    // Handles form submission for user registration
     @PostMapping("/register")
     public String register_user(@ModelAttribute Account account){
-        accountService.save(account);
-        return "redirect:/";
+        accountService.save(account);  // Calls the service layer to save the account details to the database.
+        return "redirect:/";  // Redirects the user to the homepage ("/") after successful registration.
     }
 
     @GetMapping("/login")
     public String login(Model model){
-        return "login";
+        return "login";  // Returns the name of the view (login.jsp or login.html) where the login form will be displayed.
     }
-
-
 }
