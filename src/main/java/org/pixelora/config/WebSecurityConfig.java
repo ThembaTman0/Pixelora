ce785aa1d662c508b4a322207714879780d13540
ThembaTman0 <thembatman0@gmail.com>
2024-08-19 20:36:48 +0200

    v-0.1.9

--- src/main/java/org/pixelora/config/WebSecurityConfig.java
+++ src/main/java/org/pixelora/config/WebSecurityConfig.java
@@ -12,34 +12,34 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class WebSecurityConfig {
 
-    private static final Logger logger = LoggerFactory.getLogger(WebSecurityConfig.class);
+    private static final Logger logger = LoggerFactory.getLogger(WebSecurityConfig.class); // Logger to log information and errors
 
-    private static final String[] WHITELIST = {
+    private static final String[] WHITELIST = { // List of URL patterns that are accessible without authentication
             "/",
             "/login",
             "/register",
-            "/db-console/**",
-            "/css/**",
-            "/fonts/**",
-            "/images/**",
-            "/js/**",
+            "/db-console/**",  // H2 database console access
+            "/css/**",         // Static resources: CSS files
+            "/fonts/**",       // Static resources: Font files
+            "/images/**",      // Static resources: Image files
+            "/js/**",          // Static resources: JavaScript files
     };
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        logger.info("Applying security configuration...");
+        logger.info("Applying security configuration...");  // Logs that the security configuration is being applied
 
         http.authorizeHttpRequests()
                 .requestMatchers(WHITELIST)
-                .permitAll()
+                .permitAll()           // Permits access to the URLs defined in the whitelist without authentication
                 .anyRequest()
-                .authenticated()
-                //TODO: remove these after upgrading the DB from H2 infile DB
+                .authenticated()       // Requires authentication for any other request
+                //TODO: remove these after upgrading the DB from H2 in-file DB
 
                 .and()
-                .csrf().disable()
-                .headers().frameOptions().disable();
+                .csrf().disable()      // Disables CSRF protection (should be enabled in production for security)
+                .headers().frameOptions().disable();  // Disables frame options to allow the H2 console to be loaded in a frame
 
-        return http.build();
+        return http.build();  // Builds the SecurityFilterChain with the configured security settings
     }
 }
