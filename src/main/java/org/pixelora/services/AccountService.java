ec19c8bfde4f2d5dff768437726b0431f9af25ad
ThembaTman0 <thembatman0@gmail.com>
2024-08-24 20:05:23 +0200

    v-0.2.1 Password Encryption

--- src/main/java/org/pixelora/services/AccountService.java
+++ src/main/java/org/pixelora/services/AccountService.java
@@ -3,6 +3,7 @@ package org.pixelora.services;
 import org.pixelora.model.Account;
 import org.pixelora.repositories.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,7 +11,11 @@ public class AccountService {
     @Autowired
     private AccountRepository accoutRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public Account save(Account account){
+        account.setPassword(passwordEncoder.encode(account.getPassword()));
         return accoutRepository.save(account);
     }
 }
