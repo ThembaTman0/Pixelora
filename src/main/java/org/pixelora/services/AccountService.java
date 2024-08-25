d24766c24a18ed1269a4df551690a602b3d54596
ThembaTman0 <thembatman0@gmail.com>
2024-08-25 20:52:08 +0200

    v-0.2.2 Login done

--- src/main/java/org/pixelora/services/AccountService.java
+++ src/main/java/org/pixelora/services/AccountService.java
@@ -3,11 +3,21 @@ package org.pixelora.services;
 import org.pixelora.model.Account;
 import org.pixelora.repositories.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 @Service
-public class AccountService {
+public class AccountService implements UserDetailsService {
     @Autowired
     private AccountRepository accoutRepository;
 
@@ -17,5 +27,19 @@ public class AccountService {
     public Account save(Account account){
         account.setPassword(passwordEncoder.encode(account.getPassword()));
         return accoutRepository.save(account);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Optional<Account> optionalAccount = accoutRepository.findOneByEmailIgnoreCase(email);
+        if(!optionalAccount.isPresent()){
+            throw  new UsernameNotFoundException("Account not found");
+        }
+        Account account = optionalAccount.get();
+
+        List<GrantedAuthority> grantedAuthority = new ArrayList<>();
+        grantedAuthority.add(new SimpleGrantedAuthority("Allow"));
+
+        return new User(account.getEmail(),account.getPassword(),grantedAuthority);
     }
 }
