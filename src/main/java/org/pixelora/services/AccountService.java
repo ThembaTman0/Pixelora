0fa1374764fa03336a7fc20610c2880b8db43394
ThembaTman0 <thembatman0@gmail.com>
2024-08-28 21:21:19 +0200

    v-0.2.2

--- src/main/java/org/pixelora/services/AccountService.java
+++ src/main/java/org/pixelora/services/AccountService.java
@@ -18,10 +18,10 @@ import java.util.Optional;
 
 @Service
 public class AccountService implements UserDetailsService {
-    @Autowired
+    @Autowired // Automatically injects the AccountRepository bean into this field
     private AccountRepository accoutRepository;
 
-    @Autowired
+    @Autowired // Automatically injects the AccountRepository bean into this field
     private PasswordEncoder passwordEncoder;
 
     public Account save(Account account){
@@ -40,6 +40,8 @@ public class AccountService implements UserDetailsService {
         List<GrantedAuthority> grantedAuthority = new ArrayList<>();
         grantedAuthority.add(new SimpleGrantedAuthority("Allow"));
 
+        // Return a User object containing the account's email, password, and authorities
+        // The User class implements UserDetails, which is required by Spring Security
         return new User(account.getEmail(),account.getPassword(),grantedAuthority);
     }
 }
