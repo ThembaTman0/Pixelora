ca0d8a9b5f3321284a3da46fe2e919d78435d510
ThembaTman0 <thembatman0@gmail.com>
2024-09-09 20:43:30 +0200

    v-0.2.5

--- src/main/java/org/pixelora/util/constants/Authorities.java
+++ src/main/java/org/pixelora/util/constants/Authorities.java
@@ -1,4 +1,19 @@
 package org.pixelora.util.constants;
 
-public enum Authorities {
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Authorities {
+    @Id
+    private Long Id;
+
+    private String name;
 }
