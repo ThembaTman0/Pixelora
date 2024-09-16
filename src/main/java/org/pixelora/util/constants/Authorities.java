a75507c033f9fdaf20db93869fade55cc18a6db5
ThembaTman0 <thembatman0@gmail.com>
2024-09-16 20:06:35 +0200

    v-0.2.6

--- src/main/java/org/pixelora/util/constants/Authorities.java
+++ src/main/java/org/pixelora/util/constants/Authorities.java
@@ -1,4 +1,21 @@
 package org.pixelora.util.constants;
 
 public enum Authorities {
+    RESET_ANY_USER_PASSWORD(1,"RESET_ANY_USER_PASSWORD"),
+    ACCESS_ADMIN_PANEL(2,"ACCESS_ADMIN_PANEL");
+
+    private  int authorityId;
+    private String authorityString;
+
+    private Authorities(int authorityId,String authorityString){
+        this.authorityId=authorityId;
+        this.authorityString=authorityString;
+    }
+    public int getAuthorityId() {
+        return authorityId;
+    }
+
+    public String getAuthorityString() {
+        return authorityString;
+    }
 }
